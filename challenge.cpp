619ce767afaaeef2a35cf6ae2a6c69abeaed9b8c
Tdlreis <tdlreis@gmail.com>
2023-02-13 03:04:26 +0000

    User input implemented on certificate

--- challenge.cpp
+++ challenge.cpp
@@ -12,20 +12,41 @@ void createKeysAndCertificate(){
 	MessageDigest::loadMessageDigestAlgorithms();
 	SymmetricCipher::loadSymmetricCiphersAlgorithms();
 
-	RSAKeyPair key_pair(2048);
+	RSAKeyPair key_pair(2048); 
 	RSAPublicKey *pubKey = (RSAPublicKey*) key_pair.getPublicKey();
 	RSAPrivateKey *privKey = (RSAPrivateKey*) key_pair.getPrivateKey();
 	
 	CertificateBuilder certBuilder = CertificateBuilder();
 	certBuilder.setVersion(1);
 	certBuilder.setSerialNumber(0);
+
+	string in = "";
+
+	cout << "Certificate Creation: " << endl;
+	cout << "Country (Two Letters): ";
+	getline(cin, in);
 	
 	RDNSequence rdnSubject;
-	rdnSubject.addEntry(RDNSequence::COUNTRY, "CO");
-	rdnSubject.addEntry(RDNSequence::ORGANIZATION, "organization");
-	rdnSubject.addEntry(RDNSequence::ORGANIZATION_UNIT, "oUnit");
-	rdnSubject.addEntry(RDNSequence::COMMON_NAME, "common_name");
-	
+	rdnSubject.addEntry(RDNSequence::COUNTRY, in);
+	in.clear();
+	cout << "State: ";
+	getline(cin, in);
+	rdnSubject.addEntry(RDNSequence::STATE_OR_PROVINCE, in);
+	in.clear();
+	cout << "Email: ";
+	getline(cin, in);
+	rdnSubject.addEntry(RDNSequence::EMAIL, in);
+	in.clear();
+	cout << "Title: ";
+	getline(cin, in);
+	rdnSubject.addEntry(RDNSequence::TITLE, in);
+	in.clear();
+	cout << "Full Name: ";
+	getline(cin, in);
+	string name = in;
+	in.clear();
+	rdnSubject.addEntry(RDNSequence::COMMON_NAME, name);
+
 	certBuilder.setSubject(rdnSubject);
 	certBuilder.setPublicKey(*pubKey);
 
@@ -36,15 +57,30 @@ void createKeysAndCertificate(){
 	certBuilder.setNotBefore(dateTimeNow);
 	certBuilder.setNotAfter(dateTimeExpire);
 
+
 	Certificate *cert = certBuilder.sign(*privKey, MessageDigest::SHA256);
 
-	Pkcs12Builder pkcs12Builder = Pkcs12Builder();
-	pkcs12Builder.setKeyAndCertificate(privKey, cert, "Teste");
-	pkcs12Builder.addAdditionalCert(cert);
-	
-	Pkcs12 *pkcs12 = pkcs12Builder.doFinal("202530");
+	for (size_t i = 0; i < name.size(); i++)
+	{
+		name.at(i) = tolower(name.at(i));
+		if (name.at(i) == ' ')
+		{
+			name.at(i) = '_';
+		}
+	}
 
-	std::ofstream pkcs12_file("./certificates/certificate.p12");
+	Pkcs12Builder pkcs12Builder = Pkcs12Builder();
+	pkcs12Builder.setKeyAndCertificate(privKey, cert, name);
+	pkcs12Builder.addAdditionalCert(cert);
+
+	cout << "Password: ";
+	cin >> in;
+	
+	Pkcs12 *pkcs12 = pkcs12Builder.doFinal(in);
+	in.clear();
+
+	ofstream pkcs12_file;
+	pkcs12_file.open(string("./certificates/"+name+".p12").c_str());
 	for (size_t i = 0; i < pkcs12->getDerEncoded().size(); i++)
 	{
 		pkcs12_file << pkcs12->getDerEncoded().at(i);
